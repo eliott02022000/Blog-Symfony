ec2afd05e85203766815cda92d05b864c5a766e2
Eliott <eliott.jarmoune@outlook.com>
2020-10-05 20:32:51 +0200

    commit security

--- src/Security/AppAuthentificatorAuthenticator.php
+++ src/Security/AppAuthentificatorAuthenticator.php
@@ -97,7 +97,7 @@ class AppAuthentificatorAuthenticator extends AbstractFormLoginAuthenticator imp
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
